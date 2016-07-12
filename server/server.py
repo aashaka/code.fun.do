285dd0678fa7a5681d2d05bcbb1320bd1ab0eb4d
Aashaka Shah <aashaka96@gmail.com>
2016-07-13 00:09:30 +0530

    Add proper request for classification

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api, reqparse
+from operator import itemgetter
 import os
 import json
 import requests
@@ -19,7 +20,7 @@ class GetCategories(Resource):
 			req_args = parser.parse_args()
 			name = req_args['name']
 			text = req_args['text']
-			t = text
+			txts = text
 			print name
 			data = {"text_list" : [text]}
 			data = json.dumps(data)
@@ -28,38 +29,28 @@ class GetCategories(Resource):
 				data = data,
 				headers = {'Authorization': 'Token 1569c380f608e8bd6610360be707dce71c7dcb6a',
 				'Content-Type': 'application/json'
-				#'Access-Control-Allow-Origin': 'x-requested-with',
-				#'Access-Control-Allow-Origin': '*',
-				#'Access-Control-Allow-Methods': ['POST', 'GET', 'OPTIONS', 'PUT']
 				})
 
 			response = json.loads(response.text)
-			#print response
+			print response
 			res1 = response.get('result', '')[0]
 			tags = [item['label'] for item in res1]
 			data = {"texts" : [text]}
-			args = {}
-			#args['readkey'] = 'RUVeEnp8ktFY'
-			#args['text'] = text
-			#args['output'] = 'json'
-			#args['version'] = '1.01'
-			response = requests.post('https://api.uclassify.com/v1/uclassify/topics/classify', \
-    		data = {"texts" : ["acbhcd"]}, \
-    		headers = {'Authorization': 'Token ' + "s7Rr3yE0GSvf"})
-			'''requests.post(
-				"https://api.uclassify.com/v1/uclassify/topics/classify/?",
+			data = json.dumps(data)
+			response = requests.post(
+				"https://api.uclassify.com/v1/uclassify/topics/classify??",
 				data = data,
-				headers = {'Authorization': 'Token s7Rr3yE0GSvf'})'''
+				headers = {'Authorization': 'Token RUVeEnp8ktFY'
+				})
 			response = json.loads(response.text)
-			print response
-			#res2 = response.get('classification', '')[0]
-			#print res2
-			#res2 = sorted(res2, key=res2.get, reverse=True)
+			all_responses = response[0].get('classification')
+			sortedlist = sorted(all_responses, key=itemgetter('p'), reverse=True)
+			cat = sortedlist[0].get('className')
 			result = {}
+			result['folder_name']= cat.lower()
 			result['success'] = 1
 			result['name'] = name
 			result['tags'] = tags
-			#result['folder_name'] = res2[0]
 			result['text'] = text
 			return result
 
