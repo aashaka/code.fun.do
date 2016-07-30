cc060f13169c0c1e8ad76eab1eee516407114055
Aashaka Shah <aashaka96@gmail.com>
2016-07-30 10:23:50 +0530

    Change UI, make it a single better looking page

---
+++ app/js/hide.js
@@ -0,0 +1,69 @@
+$(document).ready(function()
+	{
+		$("#declutter").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#declutter-content").css("display", "block");
+			}
+			);
+		$("#arts").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#art-content").css("display", "block");
+			}
+			);
+		$("#business").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#business-content").css("display", "block");
+			}
+			);
+		$("#computers").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#computers-content").css("display", "block");
+			}
+			);
+		$("#games").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#games-content").css("display", "block");
+			}
+			);
+		$("#health").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#health-content").css("display", "block");
+			}
+			);
+		$("#home").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#home-content").css("display", "block");
+			}
+			);
+		$("#science").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#science-content").css("display", "block");
+			}
+			);
+		$("#society").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#society-content").css("display", "block");
+			}
+			);
+		$("#sports").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#sports-content").css("display", "block");
+			}
+		);
+		$("#recreation").click(function()
+			{
+				$(".main-content").css("display", "none");
+				$("#recreation-content").css("display", "block");
+			}
+		);
+});
